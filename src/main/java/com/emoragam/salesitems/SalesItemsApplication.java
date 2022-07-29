65d72ffdcca6689d83f7748d23abda69a9feb105
Elmer Moraga <moragaelmer09@gmail.com>
2022-07-29 16:04:11 -0600

    initial commit

---
+++ src/main/java/com/emoragam/salesitems/SalesItemsApplication.java
@@ -0,0 +1,13 @@
+package com.emoragam.salesitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesItemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesItemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emoragam/salesitems/SalesItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emoragam.salesitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesItemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
